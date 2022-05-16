81011c73ef98161c7877c5d081b8c98f1d8b1e68
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-15 21:27:01 -0400

    add 5.5

--- epi_judge_python/sorted_array_remove_dups.py
+++ epi_judge_python/sorted_array_remove_dups.py
@@ -1,15 +1,34 @@
+from asyncore import write
 import functools
 from typing import List
 
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
-
+# 5/15/2022
 # Returns the number of valid entries after deletion.
 def delete_duplicates(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    # if the array is empty then return 0
+    if A is None:
+        return 0
+    
+    write_index = 0
+    
+    for i in range(1, len(A)):
+        # write_index is where you store the previous unique value
+        if A[write_index] != A[i]:
+            
+            # append the next new unique value
+            A[write_index + 1] = A[i]
+            write_index += 1
+    
+        # if the value is repeated then just keep move on, aka continue
+        
+    # return the len/number of unique value
+    return write_index + 1
+    
+    # T: O(n) because of using for loop
+    # S: O(1) because of not using any additional data structure
 
 @enable_executor_hook
 def delete_duplicates_wrapper(executor, A):
