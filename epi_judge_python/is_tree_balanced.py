5bd6a8c99469e50f85023802e32a4d4a29152f37
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-17 15:10:52 -0400

    complete is_tree_balance

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,11 +1,28 @@
+from re import sub
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+# 5/15/2022
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    def dfs(root: BinaryTreeNode) -> list[bool, int]:
+        # base case
+        if not root:
+            return [True, 0]
 
+        left_check, right_check = dfs(root.left), dfs(root.right)
+        balanced = (
+            left_check[0]
+            and right_check[0]
+            and (abs(left_check[1] - right_check[1]) <= 1)
+        )
+
+        return [balanced, 1 + max(left_check[1], right_check[1])]
+
+    return dfs(tree)[0]
+
+    # T: O(n) because we visit all the node of the tree
+    # S: O(1) because we did not use any additional ds for storage
 
 if __name__ == "__main__":
     exit(
