f4de4d47838e308bb8c9eec2c764265670c0fb78
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-17 13:52:29 -0400

    complete evaluate rpn

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,8 +2,54 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    # arr_exp = expression.split(",")
+    # s = []
+    # for i in range(len(arr_exp)):
+    #     if (
+    #         arr_exp[i] != '+'
+    #         and arr_exp[i] != '-'
+    #         and arr_exp[i] != '*'
+    #         and arr_exp[i] != '/'
+    #     ):
+    #         s.append(int(arr_exp[i]))
+    #     elif arr_exp[i] == "+":
+    #         num2 = s.pop()
+    #         num1 = s.pop()
+    #         s.append(num1 + num2)
+    #     elif arr_exp[i] == "-":
+    #         num2 = s.pop()
+    #         num1 = s.pop()
+    #         s.append(num1 - num2)
+    #     elif arr_exp[i] == "*":
+    #         num2 = s.pop()
+    #         num1 = s.pop()
+    #         s.append(num1 * num2)
+    #     elif arr_exp[i] == "/":
+    #         num2 = s.pop()
+    #         num1 = s.pop()
+    #         s.append(int(num1 / num2))
+
+    arr_exp = expression.split(",")
+    s = []
+
+    for c in arr_exp:
+        if c == "+":
+            s.append(s.pop() + s.pop())
+        elif c == "-":
+            a, b = s.pop(), s.pop()
+            s.append(b - a)
+        elif c == "*":
+            s.append(s.pop() * s.pop())
+        elif c == "/":
+            a, b = s.pop(), s.pop()
+            s.append(int(b / a))
+        else:
+            s.append(int(c))
+
+    return s[0]
+
+    # T: O(n) because we iterate through the array
+    # S: O(1) because we don't use any additional data structure to store
 
 
 if __name__ == "__main__":
