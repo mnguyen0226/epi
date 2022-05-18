27d20645380c291b94625cf225606dbec22276cc
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-18 15:16:07 -0400

    heaps are challenging

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,12 +1,32 @@
+import itertools
 from typing import Iterator, List
-
+import heapq
 from test_framework import generic_test
 
 # 5/18/2022
-# Using the min-heap
+# Using the min-heap and sliding window
 def sort_approximately_sorted_array(sequence: Iterator[int], k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    min_heap = []
+
+    # adds the first k elements into min-heap. Sort if there are fewer than k elements
+    for x in itertools.islice(sequence, k):
+        print(f"-----\n Testing: {x}")
+        heapq.heappush(min_heap, x)
+
+    result = []
+
+    # for every new element, add it to the min_heap and extract the smallest
+    for x in sequence:
+        smallest = heapq.heappushpop(min_heap, x)
+        result.append(smallest)
+
+    # sequence is exhausted, iteratively extracts the remaining elements
+
+    while min_heap:
+        smallest = heapq.heappop(min_heap)
+        result.append(smallest)
+
+    return result
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
