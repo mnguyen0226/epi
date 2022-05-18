966be449be636e8c63c60bb7e31a3e5deb6c52df
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-18 16:21:50 -0400

    complete search first key

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,10 +2,36 @@ from typing import List
 
 from test_framework import generic_test
 
-
+# 5/18/2022
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    left = 0
+    right = len(A) - 1
+    result = 0
+    while left <= right:
+        # avoid overflow
+        middle = left + (right - left) // 2
+
+        if A[middle] < k:
+            left = middle + 1
+
+        elif A[middle] == k:
+            # by this way you will save the result, not returning the middle
+            result = middle
+            right = middle - 1
+
+            # # keep traverse till get first appearance
+            # while middle != 0 and A[middle] == A[middle - 1]:
+            #     middle = middle - 1
+            # return middle
+
+        elif A[middle] > k:
+            right = middle - 1
+
+    # return -1
+    return result
+
+    # T: O(logN)
+    # S: O(1)
 
 
 if __name__ == "__main__":
