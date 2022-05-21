ec2fbc11155982e3aedffa112dc9a7e5d2236ef7
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-21 14:19:56 -0400

    complete is string palindrome

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -13,9 +13,16 @@ def is_palindromic(s: str) -> bool:
     # S: O(1)
 
 
+# 5/21/2022
+def is_palindromic2(s: str) -> bool:
+    return all(s[i] == s[~i] for i in range(len(s) // 2))
+
+    # T: O(n) if we consider going through each character in string but O(1) for doing 1 string
+    # S: O(1) since we did not use any ds for storage
+
 if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            "is_string_palindromic.py", "is_string_palindromic.tsv", is_palindromic,
+            "is_string_palindromic.py", "is_string_palindromic.tsv", is_palindromic2,
         )
     )
