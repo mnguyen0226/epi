d66114e80af4977f40f24ba39386ba6ff3d824e3
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-24 00:16:22 -0400

    done lca

--- epi_judge_python/lowest_common_ancestor.py
+++ epi_judge_python/lowest_common_ancestor.py
@@ -1,4 +1,5 @@
 import functools
+from platform import node
 from typing import Optional
 
 from binary_tree_node import BinaryTreeNode
@@ -7,12 +8,39 @@ from test_framework.binary_tree_utils import must_find_node, strip_parent_link
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
-
+# 5/23/2022
+# https://www.youtube.com/watch?v=13m9ZCB8gjw&ab_channel=TusharRoy-CodingMadeSimple
 def lca(
     tree: BinaryTreeNode, node0: BinaryTreeNode, node1: BinaryTreeNode
 ) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    # if the root node is null then no LCA
+    if tree is None:
+        return None
+
+    # if either found node is root then return root
+    if tree.data == node0.data or tree.data == node1.data:
+        return tree
+
+    # find the left and right node
+    left_node = lca(tree.left, node0, node1)
+    right_node = lca(tree.right, node0, node1)
+
+    # if both node is found then return root
+    if left_node is not None and right_node is not None:
+        return tree
+
+    # if none found then return none
+    if left_node is None and right_node is None:
+        return None
+
+    # if found only one node but not other, then the LCA must be the left or right node.
+    if left_node != None:
+        return left_node
+    
+    return right_node
+
+# T: O(n)
+# S: O(h) due to recursive calls proportional to tree height
 
 
 @enable_executor_hook
