fb749263484d2ead893f5fc5d545f27f43c9d605
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-25 11:48:25 -0400

    run black

--- epi_judge_python/path_sum.py
+++ epi_judge_python/path_sum.py
@@ -1,10 +1,27 @@
+import re
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
-
+# 5/24/2022
 def has_path_sum(tree: BinaryTreeNode, remaining_weight: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    if tree is None:
+        return False
+
+    # update the remaining weight at the current node
+    remaining_weight = remaining_weight - tree.data
+
+    # if leaf node
+    if tree.left is None and tree.right is None:
+        if remaining_weight == 0:
+            return True
+        return False
+
+    return has_path_sum(tree.left, remaining_weight) or has_path_sum(
+        tree.right, remaining_weight
+    )
+
+    # T: O(n) because we traverse through all the tree.
+    # S: O(h) becase the recursion call stack is proportional to the height of the tree.
 
 
 if __name__ == "__main__":
--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -4,14 +4,57 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+# 5/24/2022
+def inorder_traversal_recursion(tree: BinaryTreeNode) -> List[int]:
+    def helper(tree: BinaryTreeNode, arr=[]) -> BinaryTreeNode:
+        # base case
+        if tree is None:
+            return []
+
+        # if there is a node then recursive call
+        helper(tree.left, arr)
+        arr.append(tree.data)
+        helper(tree.right, arr)
+
+        # return the updated array
+        return arr
+
+    arr = helper(tree)
+
+    return arr
+
+
+# T: O(n) due to traverse through all the node
+# S: O(h) due to recursive call is proportional to the height of the tree
+
+# 5/24/2022
+def inorder_traversal_iterative(tree: BinaryTreeNode) -> List[int]:
+    stack = []
+    arr = []
+    curr = tree
+
+    while stack or curr:
+
+        # keep moving left and add all the left child
+        while curr:
+            stack.append(curr)
+            curr = curr.left
+
+        # if reach the end node, we will pop the left node, add to the result, and pop the previous left node, and move to the right
+        curr = stack.pop()  # move back the pointer to parents node
+        arr.append(curr.data)
+        curr = curr.right
+
+    return arr
+
+
+# T: O(n) because we traverse through all node
+# S: O(n) due to using the stack
 
 
 if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            "tree_inorder.py", "tree_inorder.tsv", inorder_traversal
+            "tree_inorder.py", "tree_inorder.tsv", inorder_traversal_iterative
         )
     )
