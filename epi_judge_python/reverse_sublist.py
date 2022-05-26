452ba467c0dd13b2f879f7e95e8272274c4ff85e
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-26 12:21:42 -0400

    minor change

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -1,3 +1,4 @@
+from multiprocessing import dummy
 from typing import Optional
 
 from list_node import ListNode
@@ -27,12 +28,30 @@ def reverse_sublist(L: ListNode, start: int, finish: int) -> Optional[ListNode]:
     return dummy_head.next
 
     # T: O(n)
-    # S: O(1)
+    # S: O(1) because we use exisiting node, not storing anything
 
+def reverse_sublist2(L: ListNode, start: int, finish: int) -> Optional[ListNode]:
+    dummy_head = sublist_head = ListNode(0, L)
+    
+    for _ in range(1, start): # becuase the number starts with 1
+        sublist_head = sublist_head.next
+    
+    sublist_iter = sublist_head.next
+    
+    for _ in range(finish-start):
+        temp = sublist_iter.next
+        sublist_iter.next = temp.next
+        temp.next = sublist_head.next
+        sublist_head.next = temp
+    
+    return dummy_head.next
 
+    # T: O(n)
+    # S: O(1) because we use exisiting node, not storing anything
+    
 if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            "reverse_sublist.py", "reverse_sublist.tsv", reverse_sublist
+            "reverse_sublist.py", "reverse_sublist.tsv", reverse_sublist2
         )
     )
