19310dcb5bbbca0924d11a7e0af02fdee045d799
mnguyen0226 <mnguyen0226@vt.edu>
2022-05-29 15:34:12 -0400

    done

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,9 +1,52 @@
 from test_framework import generic_test
 
-
+# 5/29/2022
 def is_letter_constructible_from_magazine(letter_text: str, magazine_text: str) -> bool:
-    # TODO - you fill in here.
+    # check for each character in letter_next, the number of appearance is not more than the one in the magazine
+    # noted that the magazine's dictionary can have more distinct letter than the letter's dictionary
+
+    # replace space
+    letter_text = letter_text.replace(" ", "")
+    magazine_text = magazine_text.replace(" ", "")
+
+    # create hashmap
+    hashmap_letter = {}
+    hashmap_magazine = {}
+
+    # store character in hashmap
+    for c in letter_text:
+        if c not in hashmap_letter:
+            hashmap_letter[c] = 1
+        else:
+            hashmap_letter[c] += 1
+
+    # store character in hashmap
+    for c in magazine_text:
+        if c not in hashmap_magazine:
+            hashmap_magazine[c] = 1
+        else:
+            hashmap_magazine[c] += 1
+
+    # go through each hashmap
+    for _ in range(len(hashmap_letter)):
+        # get the character, counter
+        char_letter, count_char_letter = hashmap_letter.popitem()
+
+        # if there is not character in the magazine hashmap
+        if char_letter not in hashmap_magazine:
+            return False
+
+        count_char_magazine = hashmap_magazine.get(char_letter)
+
+        # if the letter character counter > magain character counter, this will be wrong
+        if count_char_letter > count_char_magazine:
+            return False
+
     return True
+
+
+# T: O(n) because we traverse through character in string
+# S: O(1) because we use hashmap
 
 
 if __name__ == "__main__":
--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -30,11 +30,26 @@ def can_form_palindrome_self(s: str) -> bool:
 # T: O(n) because we go through all character in the string
 # S: O(n) because we use hash table
 
+# 5/29/2022
+def can_form_palindrome(s: str) -> bool:
+    hashmap = {}
+
+    # put all character in a hashmap
+    for c in s:
+        if c not in hashmap:
+            hashmap[c] = 1
+        else:
+            hashmap[c] += 1
+
+    # it is palindrome is the odd counted character appear only at most 1 time
+    return sum([1 for x in hashmap if hashmap[x] % 2 == 1]) <= 1
+
+
 if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
             "is_string_permutable_to_palindrome.py",
             "is_string_permutable_to_palindrome.tsv",
-            can_form_palindrome_self,
+            can_form_palindrome,
         )
     )
