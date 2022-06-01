4568ebfd6dcd48af0270e5cafe06905b822eac71
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-01 00:45:36 -0400

    done

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,11 +1,30 @@
-from typing import List
+from math import dist
+from typing import List, Dict
 
 from test_framework import generic_test
 
-
+# 5/31/2022
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    hashmap: Dict[str, int] = {}
+    min_distance = float("inf")
+
+    for i, word in enumerate(paragraph):
+        if word in hashmap:
+            # update min distance and latest index
+            min_distance = min(min_distance, i - hashmap[word])
+            hashmap[word] = i
+        else:
+            hashmap[word] = i
+
+    # we find the min distance
+    if min_distance != float("inf"):
+        return min_distance
+
+    # we did not find the min distance
+    return -1
+
+    # T: O(n) with n is the length of the array
+    # S: O(d) with d is the number of distinct words in the array stored in hashmap
 
 
 if __name__ == "__main__":
