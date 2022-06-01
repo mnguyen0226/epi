c7566a1396c661f6049788166c9732aaefc7c9df
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-01 16:48:03 -0400

    minor change

--- epi_judge_python/h_index.py
+++ epi_judge_python/h_index.py
@@ -1,11 +1,21 @@
+from re import sub
 from typing import List
 
 from test_framework import generic_test
 
-
+# We need to sort it. Draw it out.
 def h_index(citations: List[int]) -> int:
-    # TODO - you fill in here.
+    citations = sorted(citations)
+    n = len(citations)
+    for i in range(len(citations)):
+        if citations[i] >= n - i:
+            return n - i
+
     return 0
+
+
+# T: O(nlogn) due to sort
+# S: O(1) since we use not data structure to store data
 
 
 if __name__ == "__main__":
