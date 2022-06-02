144a6d3d460d18399d2450b8da3fec59381106bd
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-02 12:15:50 -0400

    done easy

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -1,41 +1,87 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
-# 5/17/2022
+# # 5/17/2022
+# class Stack:
+#     def __init__(self):
+#         self.s = []
+
+#     def empty(self) -> bool:
+#         # check the length
+#         return len(self.s) == 0
+
+#         # T: O(1)
+#         # S: O(1)
+
+#     def max(self) -> int:
+#         max = self.s[0]
+#         for i in range(len(self.s)):
+#             if self.s[i] > max:
+#                 max = self.s[i]
+#         return max
+
+#         # T: O(n)
+#         # S: O(1)
+
+#     def pop(self) -> int:
+#         last_val = self.s[-1]
+
+#         # slicing
+#         self.s = self.s[:-1]
+#         return last_val
+
+#         # T: O(1)
+#         # S: O(1)
+
+#     def push(self, x: int) -> None:
+#         self.s.append(x)
+#         return
+
+#         # T: O(1)
+#         # S: O(1)
+
+# 6/2/2022
 class Stack:
     def __init__(self):
+        """Constructor"""
         self.s = []
+        self.max_s = []  # keep track of the max stack
+
+        # T: O(1)
+        # S: O(n)
 
     def empty(self) -> bool:
-        # check the length
+        # return if the len is empty
         return len(self.s) == 0
 
         # T: O(1)
         # S: O(1)
 
     def max(self) -> int:
-        max = self.s[0]
-        for i in range(len(self.s)):
-            if self.s[i] > max:
-                max = self.s[i]
-        return max
+        return self.max_s[-1]
 
-        # T: O(n)
+        # T: O(1)
         # S: O(1)
 
     def pop(self) -> int:
-        last_val = self.s[-1]
-
-        # slicing
-        self.s = self.s[:-1]
-        return last_val
+        val = self.s[-1]
+        del self.s[-1]
+        del self.max_s[-1]
+        return val
 
         # T: O(1)
         # S: O(1)
 
     def push(self, x: int) -> None:
         self.s.append(x)
-        return
+        if len(self.max_s) == 0:
+            self.max_s.append(x)
+        else:  # compare with the largest one
+            old_max = self.max_s[-1]
+            if x > old_max:
+                self.max_s.append(x)
+            else:
+                self.max_s.append(old_max)
 
         # T: O(1)
         # S: O(1)
