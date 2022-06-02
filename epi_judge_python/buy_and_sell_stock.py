1a7707c212b4f9702758f45ca300413fb0d82865
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-02 14:37:21 -0400

    easy

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,11 +2,27 @@ from typing import List
 
 from test_framework import generic_test
 
-
+# 6/2/2022
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # we use two pointer
+    max_profit = 0
 
+    buy, sell = 0, 1
+
+    while sell != len(prices):
+        profit = prices[sell] - prices[buy]
+        if profit > 0:
+            if profit > max_profit:
+                max_profit = profit
+        else:
+            buy = sell
+        sell += 1
+
+    return max_profit
+
+
+# T: O(n) since we iterate through array
+# S: O(1) because we only use pointers
 
 if __name__ == "__main__":
     exit(
