2bcb041c6d34941169066416e90a496fdb54bd6c
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-02 16:02:09 -0400

    done

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -53,9 +53,47 @@ def is_symmetric2(tree: BinaryTreeNode) -> bool:
     # S: O(h) becase the code only run when the tree is symmetric, the callstack is proportional to the tree height. Because we dp height traversal
 
 
+def is_symmetric3(tree: BinaryTreeNode) -> bool:
+
+    if tree is None:
+        return True
+
+    def check_symmetric(tree: BinaryTreeNode):
+        # if there only root node
+        if tree.left is None and tree.right is None:
+            return (True, tree.data)  # balance status and value
+
+        # visit the left
+        left_balance, left_value = check_symmetric(tree.left)
+        if left_balance is False:
+            return False, left_value
+
+        # visit the right
+        right_balance, right_value = check_symmetric(tree.right)
+        if right_balance is False:
+            return False, right_value
+
+        # at this point, we know that left tree and right tree are balance.
+        # check current node balance by checking left right value and their subtree as well
+        new_balance = (
+            True
+            if left_value == right_value
+            and tree.left.right.data == tree.right.left.data
+            and tree.left.left.data == tree.right.right.data
+            else False
+        )
+
+        return new_balance, tree.data
+
+    return check_symmetric(tree)[0]
+
+
+# T: O(h) with h is the height of the tree
+# S: O(n) since the recursion stack is proportion to the height of the tree
+
 if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            "is_tree_symmetric.py", "is_tree_symmetric.tsv", is_symmetric2
+            "is_tree_symmetric.py", "is_tree_symmetric.tsv", is_symmetric3
         )
     )
