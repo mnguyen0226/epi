e8ebdd8fbbce83c657f646ee823e0b7d4fd9fc39
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-03 14:29:09 -0400

    minor change

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -4,26 +4,47 @@ from typing import List
 
 # 6/3/2022
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # idea is to visit the node in pre-order traversal and see if the array is sorted in order
+    # idea is to visit the node in in-order traversal and see if the array is sorted in order
     if tree is None:
         return True
 
     arr_tree = []
 
-    def pre_order_traversal_recursion(tree: BinaryTreeNode, arr_tree: List):
+    def in_order_traversal_iteration(tree: BinaryTreeNode):
+        stack = []
+        res = []
+
+        # if the current node is not null or the stack is not empty
+        while tree or stack:
+            # keep go left
+            while tree:
+                stack.append(tree)
+                tree = tree.left
+
+            # append to the result
+            tree = stack.pop()
+            res.append(tree.data)
+
+            # go right
+            tree = tree.right
+
+        return res
+
+    def in_order_traversal_recursion(tree: BinaryTreeNode, arr_tree: List):
         # if there is nothing, then we return nothing
         if tree is None:
             return None
 
         # if there is something
-        pre_order_traversal_recursion(tree.left, arr_tree)
+        in_order_traversal_recursion(tree.left, arr_tree)
         arr_tree.append(tree.data)
-        pre_order_traversal_recursion(tree.right, arr_tree)
+        in_order_traversal_recursion(tree.right, arr_tree)
 
         # if there is something, we return the updated array, this line allow us to update the input array
         return arr_tree
 
-    arr_tree = pre_order_traversal_recursion(tree, arr_tree)
+    #    arr_tree = in_order_traversal_recursion(tree, arr_tree)
+    arr_tree = in_order_traversal_iteration(tree)
 
     for i in range(0, len(arr_tree) - 1):
         if arr_tree[i] > arr_tree[i + 1]:
