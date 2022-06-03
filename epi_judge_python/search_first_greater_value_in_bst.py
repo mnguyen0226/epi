b27d41c0cfc134ecc241911b7ecaf3c8f6af13cd
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-03 16:01:01 -0400

    get un-optimized solution

--- epi_judge_python/search_first_greater_value_in_bst.py
+++ epi_judge_python/search_first_greater_value_in_bst.py
@@ -2,11 +2,38 @@ from typing import Optional
 
 from bst_node import BstNode
 from test_framework import generic_test
+import bintrees
 
-
+# 6/3/2022
+# keep popping smallest value till the value > k, then return the previous key
 def find_first_greater_than_k(tree: BstNode, k: int) -> Optional[BstNode]:
-    # TODO - you fill in here.
+    if tree is None:
+        return None
+
+    arr = []
+
+    # using inorder traversal
+    def in_order_dfs(tree, arr):
+        if tree is None:
+            return None
+        in_order_dfs(tree.left, arr)
+        arr.append(tree)
+        in_order_dfs(tree.right, arr)
+
+        return arr
+
+    # we know that the array is sorted
+    arr = in_order_dfs(tree, arr)
+
+    # linear search
+    for num in arr:
+        if num.data > k:
+            return num
+
     return None
+
+    # T: O(n) for linear search
+    # S: O(n) for array
 
 
 def find_first_greater_than_k_wrapper(tree, k):
