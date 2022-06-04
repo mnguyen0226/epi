19a10f302f91c5067f13fde7718b5e481d0d13cb
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-04 01:14:58 -0400

    optimized

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -11,7 +11,7 @@ from test_framework.test_utils import enable_executor_hook
 # 6/3/2022
 # The naive way is to store both path in 2 arrays then check when the are different
 # For BT, we can do recursion to find 2 nodes then bubble up, if a node has both True, then it must be a a LCA
-# 3 cases: - if node overlap, if both left and right noexist, if both left and right exist, if either one exist 
+# 3 cases: - if node overlap, if both left and right noexist, if both left and right exist, if either one exist
 def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
 
     # applying recursion for BT
@@ -44,11 +44,43 @@ def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
     return lca(tree, s, b)
 
 
+# T: O(n) since we visit all the node
+# S: O(h) since the call stack is proportional to the height of the tree
+
+# this method only work for BST since the tree is sorted
+def find_lca_optimized_for_bst(
+    tree: BstNode, s: BstNode, b: BstNode
+) -> Optional[BstNode]:
+    curr_node = tree
+
+    # here we are trying to get so that s/b < curr_node < b/s. Since we traverse downward, LCA is when we immediate reach this condition
+    while curr_node:
+
+        # if the current node is lesser than both nodes, then we move right
+        if curr_node.data < s.data and curr_node.data < b.data:
+            curr_node = curr_node.right
+
+        # if the current node is greater than both nodes, then we move left
+        elif curr_node.data > s.data and curr_node.data > b.data:
+            curr_node = curr_node.lefte
+
+        # if the node is one of the two values or the node is between
+        else:
+            return curr_node
+
+
+# T: O(h) / O(logN) since we use only half of the tree everytime
+# S: O(h) since the height of the call stack is proportional to the height of the tree
+
+
 @enable_executor_hook
 def lca_wrapper(executor, tree, s, b):
     result = executor.run(
         functools.partial(
-            find_lca, tree, must_find_node(tree, s), must_find_node(tree, b)
+            find_lca_optimized_for_bst,
+            tree,
+            must_find_node(tree, s),
+            must_find_node(tree, b),
         )
     )
     if result is None:
