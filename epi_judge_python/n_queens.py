c770639496e44085b6d79220a3374d00afce5e9b
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-05 13:59:53 -0400

    hard

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -1,3 +1,5 @@
+from operator import ne
+from turtle import back
 from typing import List
 
 from test_framework import generic_test
@@ -5,17 +7,53 @@ from test_framework import generic_test
 # 6/5/2022
 # Apply backtracking via bruteforce
 
-def string_to_index(string: str):
-    return 0
-
 
 def n_queens(n: int) -> List[List[int]]:
     res = []
-    
-    
-    
-    def backtracking(curr_s)
-    
+    board = []
+
+    # hashmap to keep track
+    col = set()
+    pos_diag = set()  # r + c to be constant then it is in the same positive diagonal
+    neg_diag = set()  # r - c to be constant then it is in the same negative diagonal
+
+    def backtracking(r):
+        if r == n:
+            copy = (
+                board.copy()
+            )  # make a copy of board and append so when we escape the backtracking, it can remove
+            res.append(copy)
+            return
+
+        # go through each column
+        for c in range(n):
+            if c in col or (r + c) in pos_diag or (r - c) in neg_diag:
+                continue
+
+            # update the set and board before backtrack
+            col.add(c)
+            pos_diag.add(r + c)
+            neg_diag.add(r - c)
+
+            # update the board: at the current row, what column are we adding a queen
+            board.append(c)
+
+            # backtrack recursion
+            backtracking(r + 1)
+
+            # un-update
+            col.remove(c)
+            pos_diag.remove(r + c)
+            neg_diag.remove(r - c)
+            board.remove(c)
+
+    backtracking(0)
+
+    return res
+
+
+# T: O(N!) for the first queen we consider N position, for second qeen we consider N-2 position, ... then for N queen we got N! (not N^N)
+# S: O(N^2) since we have N x N board if we don't consider the space for the output
 
 
 def comp(a, b):
