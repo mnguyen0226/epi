c187a8b89613c7a5678066acda712679e9643fa7
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-05 00:45:03 -0400

    hard backtracking

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -1,11 +1,49 @@
+from turtle import back
 from typing import List
 
 from test_framework import generic_test, test_utils
 
-
+# 6/4/2022
+# Noted that the number range is [2-9]
+# We need to use back-tracking, recursion, and a hashmap
 def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    if len(phone_number) == 0:
+        return []
+
+    hashmap = {
+        "0": "0",
+        "1": "1",
+        "2": "ABC",
+        "3": "DEF",
+        "4": "GHI",
+        "5": "JKL",
+        "6": "MNO",
+        "7": "PQRS",
+        "8": "TUV",
+        "9": "WXYZ",
+    }
+
+    # use global array
+    res = []
+
+    def backtracking(index: int, curr_string: str):
+        # base case
+        if len(curr_string) == len(phone_number):
+            res.append(curr_string)
+            return
+
+        char_list = hashmap[phone_number[index]]
+        for c in char_list:
+            # note that don't update the current string here but update at the recursive time
+            backtracking(index + 1, curr_string + c)
+
+    backtracking(0, "")
+
+    return res
+
+
+# T: O(n*4^n) = length of each solution * number of solution
+# S: O(n) if not counting for the length of the output. n is the length needed for the recursion stack
 
 
 if __name__ == "__main__":
