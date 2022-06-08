820dafb59eafee21af6d25d00c1cabc6aa94bfce
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-08 10:52:53 -0400

    reviewed

--- epi_judge_python_solutions/path_sum.py
+++ epi_judge_python_solutions/path_sum.py
@@ -14,5 +14,24 @@ def has_path_sum(tree: BinaryTreeNode, remaining_weight: int) -> bool:
     )
 
 
+def has_path_sum2(tree: BinaryTreeNode, remaining_weight: int) -> bool:
+
+    def recursion(tree, remaining_weight):
+        if tree is None:
+            return False
+        
+        # there is a node, we update the remaining weight
+        remaining_weight = remaining_weight - tree.data
+    
+        if remaining_weight == 0:
+            return True
+        
+        return recursion(tree.left, remaining_weight) and recursion(tree.right, remaining_weight)
+    
+    return recursion(tree, remaining_weight)
+
+# T: O(n) since we visit all the nodes in the worst case.
+# S: O(h) with h is the height of the tree which is proportional to the height of the reccursion call stack.
+
 if __name__ == "__main__":
-    exit(generic_test.generic_test_main("path_sum.py", "path_sum.tsv", has_path_sum))
+    exit(generic_test.generic_test_main("path_sum.py", "path_sum.tsv", has_path_sum2))
