a15a2734420a56b562412ba1b1fc68749dc43c97
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-07 20:04:50 -0400

    done medium

--- epi_judge_python_solutions/sum_root_to_leaf.py
+++ epi_judge_python_solutions/sum_root_to_leaf.py
@@ -18,9 +18,30 @@ def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
     return sum_root_to_leaf_helper(tree)
 
 
+def sum_root_to_leaf2(tree: BinaryTreeNode) -> int:
+            
+    def recursionn_pre_order(tree: BinaryTreeNode, sum):
+        if tree is None:
+            return 0
+        
+        # there is something here, we then update the sum
+        sum = sum * 2 + tree.data
+        
+        # if it is the only node, we will return that node as sum
+        if tree.left is None and tree.right is None:
+            return sum
+        
+        # if not, we will recursively update the sum accordinly to the left and right child
+        return recursionn_pre_order(tree.left, sum) + recursionn_pre_order(tree.right, sum)
+
+    return recursionn_pre_order(tree, 0)
+    
+# T: O(n) since we visit all the node
+# S: O(h) with h is the height of the tree which is proportional to the recursion call stack
+
 if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
-            "sum_root_to_leaf.py", "sum_root_to_leaf.tsv", sum_root_to_leaf
+            "sum_root_to_leaf.py", "sum_root_to_leaf.tsv", sum_root_to_leaf2
         )
     )
