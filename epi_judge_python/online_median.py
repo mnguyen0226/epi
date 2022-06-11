f8e09da7e7e38e5bc4c2ef88b189ff1ddefa3501
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-11 12:26:32 -0400

    reviewed

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -42,8 +42,49 @@ def online_median(sequence: Iterator[int]) -> List[float]:
 # S: O(n) due to using 2 heaps to store all element
 
 
+# 6/11/2022
+# Note that the median is a middle number of an array or and average
+import heapq
+
+
+def online_median2(sequence: Iterator[int]) -> List[float]:
+    result = []
+    min_heap = []
+    max_heap = []
+
+    for num in sequence:
+        heapq.heappush(max_heap, -num)
+
+        # check correct add
+        if max_heap and min_heap and -max_heap[0] > min_heap[0]:
+            wrong_added_value = -heapq.heappop(max_heap)
+            heapq.heappush(min_heap, wrong_added_value)
+
+        # check for balance
+        # len(max_heap) larger than len(min_heap) max 1
+        if len(max_heap) - len(min_heap) > 1:
+            wrong_place_value = -heapq.heappop(max_heap)
+            heapq.heappush(min_heap, wrong_place_value)
+        # len(min_heap) can't be larger than len(max_heap)
+        if len(min_heap) - len(max_heap) > 0:
+            wrong_place_value = heapq.heappop(min_heap)
+            heapq.heappush(max_heap, -wrong_place_value)
+
+        # calculate median
+        if len(max_heap) > len(min_heap):
+            result.append(-max_heap[0])
+        elif len(max_heap) == len(min_heap):
+            result.append((-max_heap[0] + min_heap[0]) / 2.0)
+
+    return result
+
+
+# T: O(nlogn) with n is the number of element in the sequence
+# S: O(logn) with n is thee number of element in the sequence
+
+
 def online_median_wrapper(sequence):
-    return online_median(iter(sequence))
+    return online_median2(iter(sequence))
 
 
 if __name__ == "__main__":
