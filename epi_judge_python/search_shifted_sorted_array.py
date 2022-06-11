1b819205712c8d0008a5498d6223456dfca198cb
mnguyen0226 <mnguyen0226@vt.edu>
2022-06-11 17:49:43 -0400

    reviewed

--- epi_judge_python/search_shifted_sorted_array.py
+++ epi_judge_python/search_shifted_sorted_array.py
@@ -34,11 +34,49 @@ def search_smallest(A: List[int]) -> int:
     return L
 
 
+# 6/11/2022
+# Using binary search, return the INDEX, not value
+def search_smallest2(A: List[int]) -> int:
+    L, R = 0, len(A) - 1
+
+    # keep track of the smallest value and index
+    smallest_value = A[0]
+    smallest_index = -1
+
+    # apply binary search
+    while L <= R:
+        # if we are in sorted portion
+        if A[L] < A[R]:
+            # update the smallest value and index
+            if A[L] <= smallest_value:
+                smallest_value = A[L]
+                smallest_index = L
+            break
+
+        # if not in sorted portion, then do binary search
+        M = L + (R - L) // 2
+
+        # update the smallest value
+        if A[M] <= smallest_value:
+            smallest_value = A[M]
+            smallest_index = M
+
+        if A[M] >= A[L]:
+            L = M + 1
+        else:
+            R = M - 1
+
+    return smallest_index
+
+
+# T: O(logn)
+# S: O(1)
+
 if __name__ == "__main__":
     exit(
         generic_test.generic_test_main(
             "search_shifted_sorted_array.py",
             "search_shifted_sorted_array.tsv",
-            search_smallest,
+            search_smallest2,
         )
     )
